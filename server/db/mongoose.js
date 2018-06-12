c49ade2267f746f5032ca98092542bde4c68e8aa
VelizarT <velizar_tonchev@abv.bg>
2018-06-12 12:28:47 +0300

    Set dev and test environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //specifies that we use the buil-in promises, not a library
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
+
+//process.env.NODE_ENV - environment; 'production' by default in heroku; 'development' for local database and 'test' for testing
