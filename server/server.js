a753ac65d2921776862e928f215e2fa96b103030
VelizarT <velizar_tonchev@abv.bg>
2018-06-13 16:37:59 +0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
+
 
 var app = express();
 const port = process.env.PORT;
@@ -94,6 +96,7 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+//Sign up route
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -113,6 +116,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+//Sign in route
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((err) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
